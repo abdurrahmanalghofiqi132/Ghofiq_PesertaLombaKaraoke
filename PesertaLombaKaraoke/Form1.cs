df9cfe59857330cb069d72ed261ee7f4333bae9d
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2026-05-01 15:15:36 +0700

    Add Form1 basic structure

--- PesertaLombaKaraoke/Form1.cs
+++ PesertaLombaKaraoke/Form1.cs
@@ -1,1 +1,19 @@
 ﻿
+using System;
+using System.Windows.Forms;
+
+namespace PesertaLombaKaraoke
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
