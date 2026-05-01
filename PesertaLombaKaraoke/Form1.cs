46ccc7abc4a4dff987e56281fd1fbe7f5c90551a
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2026-05-01 15:16:57 +0700

    Tambahkan konfigurasi  koneksi SQL

--- PesertaLombaKaraoke/Form1.cs
+++ PesertaLombaKaraoke/Form1.cs
@@ -1,16 +1,21 @@
 ﻿
 using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace PesertaLombaKaraoke
 {
     public partial class Form1 : Form
     {
-        
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+        "Data Source=LAPTOP-QN8CHOOI\\GHOFIQ;Initial Catalog=db_lomba_karaoke;Integrated Security=True";
         public Form1()
         {
 
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
         
